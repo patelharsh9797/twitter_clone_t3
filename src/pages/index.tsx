40d2cf505b07cbae674b04e68e28f2aa0d45c102
Harsh Patel <105432105+patelharsh9797@users.noreply.github.com>
2023-05-15 20:20:11 +0530

    changes in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,9 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
+          <h2 className="text-center text-5xl font-extrabold text-teal-500">
+            Hello T3!
+          </h2>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
