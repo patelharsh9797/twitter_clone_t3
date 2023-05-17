215b6130ccbb51ca8e706ecea302c3a0e3a0e6ec
Harsh Patel <105432105+patelharsh9797@users.noreply.github.com>
2023-05-17 20:11:37 +0530

    added tweetComponent and displayed all the tweets

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,5 +1,10 @@
 import InfiniteScroll from "react-infinite-scroll-component";
 import { LoadingPage, LoadingSpinner } from "./Loading";
+import Link from "next/link";
+import ProfileImage from "./ProfileImage";
+import { useSession } from "next-auth/react";
+
+import { VscHeartFilled, VscHeart } from "react-icons/vsc";
 
 type Tweet = {
   id: string;
@@ -45,11 +50,83 @@ const InfiniteTweetList = ({
         loader={<LoadingSpinner size={60} />}
       >
         {tweets.map((tweet) => {
-          return <div key={tweet.id}>{tweet.content}</div>;
+          return <TweetCard key={tweet.id} {...tweet} />;
         })}
       </InfiniteScroll>
     </ul>
   );
 };
 
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+});
+
+function TweetCard({
+  id,
+  user,
+  content,
+  cratedAt,
+  likeCount,
+  likedByMe,
+}: Tweet) {
+  return (
+    <li className="flex gap-4 border-b px-4 py-4">
+      <Link href={`/profiles/${user.id}`}>
+        <ProfileImage src={user.image} />
+      </Link>
+      <div className="flex flex-grow flex-col gap-1">
+        <div className="flex gap-1">
+          <Link
+            href={`/profiles/${user.id}`}
+            className="font-bold outline-none hover:underline focus-visible:underline"
+          >
+            {user.name}
+          </Link>
+          <span className="text-gray-500">-</span>
+          <span className="text-gray-500">
+            {dateTimeFormatter.format(cratedAt)}
+          </span>
+        </div>
+        <p className="whitespace-pre-wrap">{content}</p>
+        <HeartButton likedByMe={likedByMe} likeCount={likeCount} />
+      </div>
+    </li>
+  );
+}
+
+type HeartButtonProps = {
+  likedByMe: boolean;
+  likeCount: number;
+};
+
+function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
+  const ses = useSession();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+  if (ses.status !== "authenticated") {
+    <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+      <HeartIcon />
+      <span>{likeCount}</span>
+    </div>;
+  }
+
+  return (
+    <button
+      className={`group  flex items-center gap-1 self-start transition-colors duration-200 ${
+        likedByMe
+          ? "text-red-500"
+          : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+      }`}
+    >
+      <HeartIcon
+        className={`transition-colors duration-200 ${
+          likedByMe
+            ? "fill-red-500"
+            : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+        }`}
+      />
+      <span>{likeCount}</span>
+    </button>
+  );
+}
 export default InfiniteTweetList;
