eaebc15bdb57105d36be57fba2eeb24db218df67
Harsh Patel <105432105+patelharsh9797@users.noreply.github.com>
2023-05-23 11:34:11 +0530

    Added recent and following tweets view function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,45 @@
 import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 import InfiniteTweetList from "~/components/InfiniteTweetList";
+
+const TABS = ["Recent", "Following"] as const;
 
 import NewTweetForm from "~/components/NewTweetForm";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
+  const [selectedTab, setSelectedTab] =
+    useState<(typeof TABS)[number]>("Recent");
+
+  const session = useSession();
   return (
     <>
       <header className="sticky border-b p-4">
         <h1 className="text-2xl font-bold">Home</h1>
+        {session.status === "authenticated" && (
+          <div className="flex pt-2">
+            {TABS.map((tab) => {
+              return (
+                <button
+                  key={tab}
+                  className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
+                    tab === selectedTab
+                      ? "border-b-4 border-b-blue-500 font-bold"
+                      : ""
+                  }`}
+                  onClick={() => setSelectedTab(tab)}
+                >
+                  {tab}
+                </button>
+              );
+            })}
+          </div>
+        )}
       </header>
       <main className="">
         <NewTweetForm />
-        <RecentTweets />
+        {selectedTab === "Recent" ? <RecentTweets /> : <FollowingTweets />}
       </main>
     </>
   );
@@ -21,6 +48,23 @@ const Home: NextPage = () => {
 function RecentTweets() {
   const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
     {},
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage || false}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
+}
+
+function FollowingTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    { onlyFollowing: true },
     { getNextPageParam: (lastPage) => lastPage.nextCursor }
   );
 
